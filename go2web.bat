a7ce54d799019ed2323b926fd699e19dd1aa6228
mituvladlen <vladlen.mitu@gmail.com>
2026-03-11 17:58:59 +0200

    fix: use absolute python path in go2web.bat

--- go2web.bat
+++ go2web.bat
@@ -1,8 +1,2 @@
 @echo off
-python "%~dp0go2web.py" %* 2>nul
-if %errorlevel% neq 0 (
-    python3 "%~dp0go2web.py" %* 2>nul
-    if %errorlevel% neq 0 (
-        py "%~dp0go2web.py" %*
-    )
-)
+C:\Users\Vladlen\.platformio\python3\python.exe "%~dp0go2web.py" %*
